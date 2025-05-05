20da4288e4b9382788a4672da48cb489261ce5bf
pipetto-crypto <pipetto-crypto@gmail.com>
2025-05-05 19:08:17 +0200

    WineUtils: fix wrong mediafoundation registry keys

--- app/src/main/java/com/winlator/core/WineUtils.java
+++ app/src/main/java/com/winlator/core/WineUtils.java
@@ -239,18 +239,17 @@ public abstract class WineUtils {
                     registryEditor.setStringValue("Software\\Classes\\CLSID\\{10FEF81C-0DAA-4AF0-B714-1F1689C08C8C}\\InprocServer32", null, "C:\\windows\\system32\\msmpeg2vdec.dll");
                     registryEditor.setStringValue("Software\\Classes\\CLSID\\{32D186A7-218F-4C75-8876-DD77273A8999}\\InprocServer32", null, "C:\\windows\\system32\\msmpeg2adec.dll");
                     registryEditor.setStringValue("Software\\Classes\\CLSID\\{62CE7E72-4C71-4D20-B15D-452831A87D9D}\\InprocServer32", null, "C:\\windows\\system32\\msmpeg2vdec.dll");
-                    registryEditor.setStringValue("Software\\Wow6432Node\\Classes\\CLSID\\{271C3902-6095-4C45-A22F-20091816EE9E}\\InprocServer32", null, "C:\\windows\\syswow64\\mf.dll");
-                    registryEditor.setStringValue("Software\\Wow6432Node\\Classes\\CLSID\\{2EEB4ADF-4578-4D10-BCA7-BB955F56320A}\\InprocServer32", null, "C:\\windows\\syswow64\\wmadmod.dll");
-                    registryEditor.setStringValue("Software\\Wow6432Node\\Classes\\CLSID\\{5210F8E4-B0BB-47C3-A8D9-7B2282CC79ED}\\InprocServer32", null, "C:\\windows\\syswow64\\wmadmod.dll");
-                    registryEditor.setStringValue("Software\\Wow6432Node\\Classes\\CLSID\\{82D353DF-90BD-4382-8BC2-3F6192B76E34}\\InprocServer32", null, "C:\\windows\\syswow64\\wmvdecod.dll");
-                    registryEditor.setStringValue("Software\\Wow6432Node\\Classes\\CLSID\\{98230571-0087-4204-B020-3282538E57D3}\\InprocServer32", null, "C:\\windows\\syswow64\\colorcnv.dll");
-                    registryEditor.setStringValue("Software\\Wow6432Node\\Classes\\CLSID\\{477EC299-1421-4BDD-971F-7CCB933F21AD}\\InprocServer32", null, "C:\\windows\\syswow64\\mf.dll");
-                    registryEditor.setStringValue("Software\\Wow6432Node\\Classes\\CLSID\\{48E2ED0F-98C2-4A37-BED5-166312DDD83F}\\InprocServer32", null, "C:\\windows\\syswow64\\mfreadwrite.dll");
-                    registryEditor.setStringValue("Software\\Wow6432Node\\Classes\\CLSID\\{10FEF81C-0DAA-4AF0-B714-1F1689C08C8C}\\InprocServer32", null, "C:\\windows\\syswow64\\msmpeg2vdec.dll");
-                    registryEditor.setStringValue("Software\\Wow6432Node\\Classes\\CLSID\\{32D186A7-218F-4C75-8876-DD77273A8999}\\InprocServer32", null, "C:\\windows\\syswow64\\msmpeg2adec.dll");
-                    registryEditor.setStringValue("Software\\Wow6432Node\\Classes\\CLSID\\{62CE7E72-4C71-4D20-B15D-452831A87D9D}\\InprocServer32", null, "C:\\windows\\syswow64\\msmpeg2vdec.dll");
-                }
-                else {
+                    registryEditor.setStringValue("Software\\Classes\\Wow6432Node\\CLSID\\{271C3902-6095-4C45-A22F-20091816EE9E}\\InprocServer32", null, "C:\\windows\\syswow64\\mf.dll");
+                    registryEditor.setStringValue("Software\\Classes\\Wow6432Node\\CLSID\\{2EEB4ADF-4578-4D10-BCA7-BB955F56320A}\\InprocServer32", null, "C:\\windows\\syswow64\\wmadmod.dll");
+                    registryEditor.setStringValue("Software\\Classes\\Wow6432NOde\\CLSID\\{5210F8E4-B0BB-47C3-A8D9-7B2282CC79ED}\\InprocServer32", null, "C:\\windows\\syswow64\\wmadmod.dll");
+                    registryEditor.setStringValue("Software\\Classes\\Wow6432Node\\CLSID\\{82D353DF-90BD-4382-8BC2-3F6192B76E34}\\InprocServer32", null, "C:\\windows\\syswow64\\wmvdecod.dll");
+                    registryEditor.setStringValue("Software\\Classes\\Wow6432Node\\CLSID\\{98230571-0087-4204-B020-3282538E57D3}\\InprocServer32", null, "C:\\windows\\syswow64\\colorcnv.dll");
+                    registryEditor.setStringValue("Software\\Classes\\Wow6432Node\\CLSID\\{477EC299-1421-4BDD-971F-7CCB933F21AD}\\InprocServer32", null, "C:\\windows\\syswow64\\mf.dll");
+                    registryEditor.setStringValue("Software\\Classes\\Wow6432Node\\CLSID\\{48E2ED0F-98C2-4A37-BED5-166312DDD83F}\\InprocServer32", null, "C:\\windows\\syswow64\\mfreadwrite.dll");
+                    registryEditor.setStringValue("Software\\Classes\\Wow6432Node\\CLSID\\{10FEF81C-0DAA-4AF0-B714-1F1689C08C8C}\\InprocServer32", null, "C:\\windows\\syswow64\\msmpeg2vdec.dll");
+                    registryEditor.setStringValue("Software\\Classes\\Wow6432Node\\CLSID\\{32D186A7-218F-4C75-8876-DD77273A8999}\\InprocServer32", null, "C:\\windows\\syswow64\\msmpeg2adec.dll");
+                    registryEditor.setStringValue("Software\\Classes\\Wow6432Node\\CLSID\\{62CE7E72-4C71-4D20-B15D-452831A87D9D}\\InprocServer32", null, "C:\\windows\\syswow64\\msmpeg2vdec.dll");
+                } else {
                     registryEditor.setStringValue("Software\\Classes\\CLSID\\{271C3902-6095-4C45-A22F-20091816EE9E}\\InprocServer32", null, "C:\\windows\\system32\\winegstreamer.dll");
                     registryEditor.setStringValue("Software\\Classes\\CLSID\\{2EEB4ADF-4578-4D10-BCA7-BB955F56320A}\\InprocServer32", null, "C:\\windows\\system32\\winegstreamer.dll");
                     registryEditor.setStringValue("Software\\Classes\\CLSID\\{5210F8E4-B0BB-47C3-A8D9-7B2282CC79ED}\\InprocServer32", null, "C:\\windows\\system32\\winegstreamer.dll");
@@ -261,16 +260,16 @@ public abstract class WineUtils {
                     registryEditor.setStringValue("Software\\Classes\\CLSID\\{10FEF81C-0DAA-4AF0-B714-1F1689C08C8C}\\InprocServer32", null, "C:\\windows\\system32\\winegstreamer.dll");
                     registryEditor.setStringValue("Software\\Classes\\CLSID\\{32D186A7-218F-4C75-8876-DD77273A8999}\\InprocServer32", null, "C:\\windows\\system32\\winegstreamer.dll");
                     registryEditor.setStringValue("Software\\Classes\\CLSID\\{62CE7E72-4C71-4D20-B15D-452831A87D9D}\\InprocServer32", null, "C:\\windows\\system32\\winegstreamer.dll");
-                    registryEditor.setStringValue("Software\\Wow6432Node\\Classes\\CLSID\\{271C3902-6095-4C45-A22F-20091816EE9E}\\InprocServer32", null, "C:\\windows\\system32\\winegstreamer.dll");
-                    registryEditor.setStringValue("Software\\Wow6432Node\\Classes\\CLSID\\{2EEB4ADF-4578-4D10-BCA7-BB955F56320A}\\InprocServer32", null, "C:\\windows\\system32\\winegstreamer.dll");
-                    registryEditor.setStringValue("Software\\Wow6432Node\\Classes\\CLSID\\{5210F8E4-B0BB-47C3-A8D9-7B2282CC79ED}\\InprocServer32", null, "C:\\windows\\system32\\winegstreamer.dll");
-                    registryEditor.setStringValue("Software\\Wow6432Node\\Classes\\CLSID\\{82D353DF-90BD-4382-8BC2-3F6192B76E34}\\InprocServer32", null, "C:\\windows\\system32\\winegstreamer.dll");
-                    registryEditor.setStringValue("Software\\Wow6432Node\\Classes\\CLSID\\{98230571-0087-4204-B020-3282538E57D3}\\InprocServer32", null, "C:\\windows\\system32\\winegstreamer.dll");
-                    registryEditor.setStringValue("Software\\Wow6432Node\\Classes\\CLSID\\{477EC299-1421-4BDD-971F-7CCB933F21AD}\\InprocServer32", null, "C:\\windows\\system32\\winegstreamer.dll");
-                    registryEditor.setStringValue("Software\\Wow6432Node\\Classes\\CLSID\\{48E2ED0F-98C2-4A37-BED5-166312DDD83F}\\InprocServer32", null, "C:\\windows\\system32\\winegstreamer.dll");
-                    registryEditor.setStringValue("Software\\Wow6432Node\\Classes\\CLSID\\{10FEF81C-0DAA-4AF0-B714-1F1689C08C8C}\\InprocServer32", null, "C:\\windows\\system32\\winegstreamer.dll");
-                    registryEditor.setStringValue("Software\\Wow6432Node\\Classes\\CLSID\\{32D186A7-218F-4C75-8876-DD77273A8999}\\InprocServer32", null, "C:\\windows\\system32\\winesgtreamer.dll");
-                    registryEditor.setStringValue("Software\\Wow6432Node\\Classes\\CLSID\\{62CE7E72-4C71-4D20-B15D-452831A87D9D}\\InprocServer32", null, "C:\\windows\\system32\\winegstreamer.dll");
+                    registryEditor.setStringValue("Software\\Classes\\Wow6432Node\\CLSID\\{271C3902-6095-4C45-A22F-20091816EE9E}\\InprocServer32", null, "C:\\windows\\system32\\winegstreamer.dll");
+                    registryEditor.setStringValue("Software\\Classes\\Wow6432Node\\CLSID\\{2EEB4ADF-4578-4D10-BCA7-BB955F56320A}\\InprocServer32", null, "C:\\windows\\system32\\winegstreamer.dll");
+                    registryEditor.setStringValue("Software\\Classes\\Wow6432Node\\CLSID\\{5210F8E4-B0BB-47C3-A8D9-7B2282CC79ED}\\InprocServer32", null, "C:\\windows\\system32\\winegstreamer.dll");
+                    registryEditor.setStringValue("Software\\Classes\\Wow6432Node\\CLSID\\{82D353DF-90BD-4382-8BC2-3F6192B76E34}\\InprocServer32", null, "C:\\windows\\system32\\winegstreamer.dll");
+                    registryEditor.setStringValue("Software\\Classes\\Wow6432Node\\CLSID\\{98230571-0087-4204-B020-3282538E57D3}\\InprocServer32", null, "C:\\windows\\system32\\winegstreamer.dll");
+                    registryEditor.setStringValue("Software\\Classes\\Wow6432Node\\CLSID\\{477EC299-1421-4BDD-971F-7CCB933F21AD}\\InprocServer32", null, "C:\\windows\\system32\\winegstreamer.dll");
+                    registryEditor.setStringValue("Software\\Classes\\Wow6432Node\\CLSID\\{48E2ED0F-98C2-4A37-BED5-166312DDD83F}\\InprocServer32", null, "C:\\windows\\system32\\winegstreamer.dll");
+                    registryEditor.setStringValue("Software\\Classes\\Wow6432Node\\CLSID\\{10FEF81C-0DAA-4AF0-B714-1F1689C08C8C}\\InprocServer32", null, "C:\\windows\\system32\\winegstreamer.dll");
+                    registryEditor.setStringValue("Software\\Classes\\Wow6432Node\\CLSID\\{32D186A7-218F-4C75-8876-DD77273A8999}\\InprocServer32", null, "C:\\windows\\system32\\winesgtreamer.dll");
+                    registryEditor.setStringValue("Software\\Classes\\Wow6432Node\\CLSID\\{62CE7E72-4C71-4D20-B15D-452831A87D9D}\\InprocServer32", null, "C:\\windows\\system32\\winegstreamer.dll");
                 }
             }
         }
