a6ace025c6761efc51919d3627345ca27350d363
Your Name <you@example.com>
2026-04-29 13:58:10 +0000

    Update graphics drivers and DXVK defaults

--- app/src/main/java/com/winlator/cmod/core/DefaultVersion.java
+++ app/src/main/java/com/winlator/cmod/core/DefaultVersion.java
@@ -2,10 +2,10 @@ package com.winlator.cmod.core;
 
 public abstract class DefaultVersion {
     public static final String BOX86 = "0.3.2";
-    public static final String BOX64 = "0.3.6";
+    public static final String BOX64 = "0.4.2";
     public static final String FEXCORE = "2505";
     public static final String WRAPPER = "System";
-    public static final String DXVK = "2.3.1";
+    public static final String DXVK = "async-1.10.3";
     public static final String D8VK = "1.0";
     public static final String VKD3D = "2.12-0";
 }
